7b6a53c064c0410dc8e16b630ca334257b57fb52
xanderdj <x.de.j@hotmail.com>
2018-10-08 01:57:59 +0100

    you can now use tags to limit the randomness. pass these through as commandline arguments

--- RandomMoviePicker.py
+++ RandomMoviePicker.py
@@ -1,6 +1,7 @@
 import os
 import re
 import random
+import sys
 
 
 def get_files_that_end_with(path, end_string):
@@ -18,8 +19,18 @@ def get_files_that_end_with(path, end_string):
 
 def start_random_video():
     videos = get_files_that_end_with(os.getcwd(), ".mp4")
+    tags = []
+    filteredvideos = []
+    if len(sys.argv) > 1:
+        for x in range(1, len(sys.argv)):
+            tag = sys.argv[x]
+            tags.append(tag)
+            f = lambda vid: tag in vid
+            filteredvideos.extend(list(filter(f, videos)))
+
+    videos = filteredvideos if (len(sys.argv) > 1) else videos
     length = len(videos)
-    random_vid = videos[random.randint(0,length-1)]
+    random_vid = videos[random.randint(0, length - 1)]
     os.startfile(os.getcwd() + "/" + random_vid)
 
 
